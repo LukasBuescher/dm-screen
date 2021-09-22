166f4b11681b8108d065a205af47ad3e160f968e
Lukas Büscher <buescherlukas@gmail.com>
2021-09-22 20:45:13 +0200

    Redirect to id 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'character/:id', component: CharacterdetailsComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/character/1', pathMatch: 'full' },
 ];
 
 @NgModule({
