68faabf2059bfbaba2b8eaf6dfcf17c8b659bc8f
Lukas Büscher <buescherlukas@gmail.com>
2021-10-16 13:02:06 +0200

    Display stat tab

--- src/app/characterdetails/characterdetails.component.ts
+++ src/app/characterdetails/characterdetails.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Character } from '../character';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../services/character.service';
-import { CharacterAction } from '../characteraction';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -14,18 +13,7 @@ export class CharacterdetailsComponent implements OnInit, OnDestroy {
 
   character: Character | undefined;
   private _routerSubscription: Subscription | undefined;
-
-  otheractions: CharacterAction[] =[
-    {"name":"Dash", "text":"Double your Movement"},
-    {"name":"Disengage", "text":"Stop provoking Opportunity Attacks"},
-    {"name":"Dodge", "text":"Become harder to hit"},
-    {"name":"Help", "text":"Give an ally advantage"},
-    {"name":"Hide", "text":"Attempt to hide"},
-    {"name":"Ready", "text":"Prepare an action"},        
-    {"name":"Use Skill", "text":"Attempt to use on of your skills"},        
-    {"name":"Shove/Grapple", "text":"Push creature away ore hold them in place"},
-    {"name":"Interact with Object", "text":"Interact with an Object in the environment or your inventory sdasodsahdasöv ljdhasl dasjlhdasjlhdhskladäskladskla jdlskadksajdskajdklasjdkasjdlkj lkdskjdlkasjdklsajdlasdkljdlkajdskljdlkasjd skjdsklajdklsajdklsajdlksa" }
-  ]            
+            
   constructor(
     private route: ActivatedRoute,
     private characterService: CharacterService,
@@ -35,7 +23,6 @@ export class CharacterdetailsComponent implements OnInit, OnDestroy {
     this._routerSubscription = this.route.paramMap.subscribe(
       result => {
         const id = String(result.get('id'));
-        console.log(id)
         this.characterService.getCharacter(id).subscribe(
           char => this.character = char
         )  
