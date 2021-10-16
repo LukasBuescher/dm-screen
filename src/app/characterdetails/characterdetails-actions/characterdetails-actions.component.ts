77edec423c44abdfa44c8dbfdeffe4065ff16b69
Lukas Büscher <buescherlukas@gmail.com>
2021-10-16 13:08:26 +0200

    Move general actions to shared folder

--- src/app/characterdetails/characterdetails-actions/characterdetails-actions.component.ts
+++ src/app/characterdetails/characterdetails-actions/characterdetails-actions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Character } from 'src/app/character';
 import { CharacterAction } from 'src/app/characteraction';
 import { Input } from '@angular/core';
+import { generalActions } from 'src/app/shared/constants/general-actions';
 
 @Component({
   selector: 'app-characterdetails-actions',
@@ -11,17 +12,7 @@ import { Input } from '@angular/core';
 export class CharacterdetailsActionsComponent implements OnInit {
 
   
-  otheractions: CharacterAction[] =[
-    {"name":"Dash", "text":"Double your Movement"},
-    {"name":"Disengage", "text":"Stop provoking Opportunity Attacks"},
-    {"name":"Dodge", "text":"Become harder to hit"},
-    {"name":"Help", "text":"Give an ally advantage"},
-    {"name":"Hide", "text":"Attempt to hide"},
-    {"name":"Ready", "text":"Prepare an action"},        
-    {"name":"Use Skill", "text":"Attempt to use on of your skills"},        
-    {"name":"Shove/Grapple", "text":"Push creature away ore hold them in place"},
-    {"name":"Interact with Object", "text":"Interact with an Object in the environment or your inventory" }
-  ]
+  generalActions: CharacterAction[] = generalActions;
 
   @Input() character: Character | undefined;
 
---
+++ src/app/shared/constants/general-actions.ts
@@ -0,0 +1,13 @@
+import { CharacterAction } from "src/app/characteraction";
+
+export const generalActions: CharacterAction[] = [
+    {"name":"Dash", "text":"Double your Movement"},
+    {"name":"Disengage", "text":"Stop provoking Opportunity Attacks"},
+    {"name":"Dodge", "text":"Become harder to hit"},
+    {"name":"Help", "text":"Give an ally advantage"},
+    {"name":"Hide", "text":"Attempt to hide"},
+    {"name":"Ready", "text":"Prepare an action"},        
+    {"name":"Use Skill", "text":"Attempt to use on of your skills"},        
+    {"name":"Shove/Grapple", "text":"Push creature away ore hold them in place"},
+    {"name":"Interact with Object", "text":"Interact with an Object in the environment or your inventory" }
+  ]
